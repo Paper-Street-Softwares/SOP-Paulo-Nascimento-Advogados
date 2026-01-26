c716ca33e5b929e1b66873ef3c5b49ecbced3092
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-26 10:34:11 -0300

    fix: Logo e dimensões

--- src/components/sections/NavbarNovaTemplate.jsx
+++ src/components/sections/NavbarNovaTemplate.jsx
@@ -71,8 +71,8 @@ function NavbarNovaTemplate({
           <div
             className={`flex flex-col z-20 relative  ${
               isScrolled
-                ? 'w-[20%] tablet1:w-[20%] tablet2:w-[15%] desktop1:w-[15%] desktop3:w-[10%] transition-all duration-700'
-                : 'w-[40%] phone2:w-[30%] phone3:w-[25%] tablet1:w-[30%] tablet2:w-[20%] desktop1:w-[20%] desktop3:w-[15%] transition-all duration-700'
+                ? 'w-[20%] tablet1:w-[20%] tablet2:w-[15%] desktop1:w-[15%] desktop2:w-[12%] desktop3:w-[10%] transition-all duration-700'
+                : 'w-[40%] phone2:w-[30%] phone3:w-[25%] tablet1:w-[30%] tablet2:w-[20%] desktop1:w-[18%] desktop2:w-[15%] desktop3:w-[15%] transition-all duration-700'
             }`}
           >
             {' '}
